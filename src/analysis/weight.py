c86cd7f69bbbbbd968bccc43033dd6b6c897883c
RintaroKARASHIMA <rintaro.karashima@gmail.com>
2024-03-22 22:58:46 +0900

    Create package for weight reg_num

---
+++ src/analysis/weight.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+
+def by_classification(
+                            df: pd.DataFrame,
+                            classification: str
+                            ):
+    class_weight_df = (
+        df.copy()
+        .groupby(['right_person_name', 'reg_num'])[[classification]]
+        .nunique()
+        .reset_index(drop=False)
+        .rename(columns={classification: 'class_weight'})
+    )
+
+    # ipc_weight_df['weight'] = round(1 / ipc_weight_df['weight'], 2)
+
+    class_weight_df = pd.merge(
+        df, class_weight_df,
+        on=['reg_num', 'right_person_name'],
+        how='left'
+    )
+    return class_weight_df
+
+def by_applicant(
+                        df: pd.DataFrame
+                        ):
+    applicant_weight_df = (
+            df.copy()
+            .groupby(['reg_num'])[['right_person_name']]
+            .nunique()
+            .reset_index(drop=False)
+            .rename(columns={'right_person_name': 'applicant_weight'})
+        )
+
+        # applicant_weight_df['weight'] = round(1 / applicant_weight_df['weight'], 2)
+
+    applicant_weight_df = pd.merge(
+        df, applicant_weight_df,
+        on=['reg_num', 'right_person_name'],
+        how='left'
+    )
+
+    return applicant_weight_df
---
+++ src/initial_condition.py
@@ -0,0 +1,17 @@
+# src/variable.py
+
+
+## 初期条件
+AR = 'app'  # app or reg
+YEAR_STYLE = 'year'  # nendo or year
+
+YEAR_START = 1981  # int
+YEAR_END = 2010  # int
+YEAR_RANGE = 10  # int
+
+CLASSIFICATION = 'schmoch_35'  # ipc3 or ipc4 or schmoch
+CLASS_WEIGHT = 'fraction'  # fraction or duplication
+APPLICANT_WEIGHT = 'fraction'  # fraction or duplication
+
+EXTRACT_POPULATION = 'all'  # all or sep_year
+TOP_P_OR_NUM = ('num', 1000)  # (p or num, int)
