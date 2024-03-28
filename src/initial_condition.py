f0e6b2335c88ec9a2a47cf7735ef131dc056310a
RintaroKARASHIMA <rintaro.karashima@gmail.com>
2024-03-28 14:17:16 +0900

    Slight fix

--- src/initial_condition.py
+++ src/initial_condition.py
@@ -2,16 +2,50 @@
 
 
 ## 初期条件
-AR = 'app'  # app or reg
-YEAR_STYLE = 'year'  # nendo or year
+AR = "app"  # app or reg
+YEAR_STYLE = "year"  # nendo or year
 
 YEAR_START = 1981  # int
 YEAR_END = 2010  # int
 YEAR_RANGE = 10  # int
 
-CLASSIFICATION = 'schmoch35'  # ipc3 or ipc4 or schmoch
-CLASS_WEIGHT = 'fraction'  # fraction or duplication
-APPLICANT_WEIGHT = 'fraction'  # fraction or duplication
+CLASSIFICATION = "schmoch35"  # ipc3 or ipc4 or schmoch
+CLASS_WEIGHT = "fraction"  # fraction or duplication
+APPLICANT_WEIGHT = "fraction"  # fraction or duplication
 
-EXTRACT_POPULATION = 'all'  # all or sep_year
-TOP_P_OR_NUM = ('p', 1)  # (p or num, int)
+EXTRACT_POPULATION = "all"  # all or sep_year
+TOP_P_OR_NUM = ('num', 500)  # (p or num, int)
+
+COLOR_LIST = [
+    "turquoise",
+    "gold",
+    "lime",
+    "indigo",
+    "red",
+    "coral",
+    "navy",
+    "skyblue",
+    "tomato",
+    "olive",
+    "cyan",
+    "darkred",
+    "darkgreen",
+    "darkblue",
+    "darkorange",
+    "darkviolet",
+    "deeppink",
+    "firebrick",
+    "darkcyan",
+    "darkturquoise",
+    "darkslategray",
+    "darkgoldenrod",
+    "mediumblue",
+    "mediumseagreen",
+    "mediumpurple",
+    "mediumvioletred",
+    "midnightblue",
+    "saddlebrown",
+    "seagreen",
+    "sienna",
+    "steelblue",
+]
