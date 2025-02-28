e65817968d2ce4c484458dcde6e47023d75a06fc
RintaroKARASHIMA <rintaro.karashima@gmail.com>
2025-02-28 19:57:35 +0900

    Organized load_libraries and initial_conditions

--- src/initialize/initial_conditions.py
+++ src/initialize/initial_conditions.py
@@ -2,7 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-## 初期条件
+# COUNTRY = 'JP'  # JP or EU
+
 AR = 'app'  # app or reg
 YEAR_STYLE = 'nendo'  # nendo or year
 
@@ -10,21 +11,28 @@ YEAR_START = 1981  # int
 YEAR_END = 2010  # int
 YEAR_RANGE = 5  # int
 
-COUNTRY = 'JP'  # JP or EU
 
-REGION_CORPORATION = 'right_person_name' # right_person_name or right_person_addr
-CLASSIFICATION = 'ipc3'  # ipc3 or ipc4 or schmoch
-VALUE = 'reg_num'
+REGION_CORPORATION = 'right_person_name'  # right_person_name or right_person_addr
+CLASSIFICATION = 'schmoch35'  # ipc3 or ipc4 or schmoch35
+if 'ipc' in CLASSIFICATION: DIGIT = int(CLASSIFICATION[-1])
+
+VALUE = 'reg_num'  # reg_num or app_num
 CLASS_WEIGHT = 'fraction'  # fraction or duplication
 APPLICANT_WEIGHT = 'fraction'  # fraction or duplication
 
-# reg_num filter
 EXTRACT_POPULATION = 'all'  # all or sep_year
 TOP_P_OR_NUM = ('p', 3)  # (p or num, int)
 
-PERIOD_ORDER_DICT = {f'{YEAR_START}-{YEAR_START+YEAR_RANGE-1}': i for i, YEAR_START in enumerate(range(YEAR_START, YEAR_END+1, YEAR_RANGE))} 
-PERIOD_ORDER_DICT[f'{YEAR_START}-{YEAR_END}'] = len(PERIOD_ORDER_DICT)
+PERIOD_ORDER_DICT = {f'{YEAR_START}-{YEAR_END}' : 0} | \
+                    {f'{YEAR_START}-{YEAR_START+YEAR_RANGE-1}': i+1 for i,
+                     YEAR_START in enumerate(range(YEAR_START, YEAR_END+1, YEAR_RANGE))}
 
-# INITIAL_CONDITION = 
+CONDITION = f'{AR}_{YEAR_STYLE}_{YEAR_START}_{YEAR_END}_{YEAR_RANGE}_{EXTRACT_POPULATION}_{TOP_P_OR_NUM[0]}_{TOP_P_OR_NUM[1]}_{REGION_CORPORATION}_{APPLICANT_WEIGHT}_{CLASSIFICATION}_{CLASS_WEIGHT}'
 
-
+OUTPUT_DIR = '../../outputs/'
+RAW_IN_DIR = '../../data/raw/internal/'
+RAW_EX_DIR = '../../data/raw/external/'
+IN_IN_DIR = '../../data/interim/internal/'
+IN_EX_DIR = '../../data/interim/external/'
+PRO_IN_DIR = '../../data/processed/internal/'
+PRO_EX_DIR = '../../data/processed/external/'
--- src/initialize/load_libraries.py
+++ src/initialize/load_libraries.py
@@ -1,21 +1,34 @@
-#! (root)/notebooks/00_template/0_LoadLibraries.py python3
+#! (root)/src/initialize/load_libraries.py python3
 # -*- coding: utf-8 -*-
 
 ## Import Library
-### Processing Data
+### Systems
+from importlib import reload
+from glob import glob
+import os
 import sys
-import pandas as pd
+
+### Processing Data
 import numpy as np
+import pandas as pd
 
 ### Visualization
 from IPython.display import display
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ptick
 
 ### Third Party
+from ecomplexity import ecomplexity, proximity
+
+## Import Original Modules
+sys.path.append('../../src')
+from calculation import weight
+from visualize import rank as vr
 
 ### Set Visualization Parameters
 pd.options.display.float_format = '{:.3f}'.format
 
-## Import Original Modules
-sys.path.append('../../src')
-
-#%%
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['font.family'] = 'Meiryo'
+plt.rcParams['font.size'] = 25
+plt.rcParams.update({'figure.autolayout': True})
